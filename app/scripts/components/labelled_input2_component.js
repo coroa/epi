77c4f3256f333f80457e9088576c00b6233b9a2a
Jonas Hoersch <jonas@chaoflow.net>
2014-06-04 15:02:39 +0200

    labelled-input{,2} components with defaults and reset

--- app/scripts/components/labelled_input2_component.js
+++ app/scripts/components/labelled_input2_component.js
@@ -1,16 +1,49 @@
+var make_input_view = function(which) {
+        return Ember.TextField.extend({
+            valueBinding: 'parentView.value' + which,
+            classNames: ['form-control'],
+            init: function() {
+                if (this.get('parentView')
+                    .hasOwnProperty('default' + which + 'Binding'))
+                {
+                    this.set('classNameBindings',
+                             ['parentView.isCustom' + which + ':btn-warning:btn-success']);
+                }
+                this._super();
+            },
+            willInsertElement: function() {
+                this.set('parentView.labelFor' + which, this.get('elementId'));
+            }
+        });
+    },
+    make_is_custom = function(which) {
+        return function() {
+            return this.hasOwnProperty('default' + which + 'Binding')
+                && this.get('value' + which) != this.get('default' + which);
+        }.property('value' + which, 'default' + which);
+    };
+
 App.LabelledInput2Component = Ember.Component.extend({
     classNames: ['form-group'],
+    labelFor1: null,
+    labelFor2: null,
+    isCustom1: make_is_custom("1"),
+    isCustom2: make_is_custom("2"),
 
     init: function() {
         this.set('layoutName',
                  "components/labelled-input2-"
-                 + (this.get('addon') !== undefined ? 'wA' : 'woA') + '-'
-                 + ((this.get('isCustom1') !== undefined &&
-                     this.get('isCustom2') !== undefined) ? 'wC' :
-                    'woC'));
+                 + (this.get('addon') !== undefined ? 'wA' : 'woA'));
         this._super.apply(this, arguments);
+    },
+    inputView1: make_input_view('1'),
+    inputView2: make_input_view('2'),
+
+    actions: {
+        reset: function(which) {
+            this.set('value' + which, this.get('default' + which));
+        }
     }
-    // layoutName: 'components/labelled-input-woA-woC',
 });
 
 Ember.Handlebars.helper('labelled-input2', App.LabelledInput2Component);
--- app/scripts/components/labelled_input_component.js
+++ app/scripts/components/labelled_input_component.js
@@ -1,14 +1,38 @@
 App.LabelledInputComponent = Ember.Component.extend({
     classNames: ['form-group'],
+    classNameBindings: ['isCustom:has-feedback'],
+    labelFor: null,
+    isCustom: function() {
+        return this.hasOwnProperty('defaultBinding')
+            && this.get('value') != this.get('default');
+    }.property('value', 'default'),
 
     init: function() {
         this.set('layoutName',
                  "components/labelled-input-"
-                 + (this.get('addon') !== undefined ? 'wA' : 'woA') + '-'
-                 + (this.get('isCustom') !== undefined ? 'wC' : 'woC'));
-        this._super.apply(this, arguments);
+                 + (this.get('addon') !== undefined ? 'wA' : 'woA'));
+        this._super();
+    },
+    inputView: Ember.TextField.extend({
+        valueBinding: 'parentView.value',
+        classNames: ['form-control'],
+        init: function() {
+            if (this.get('parentView').hasOwnProperty('defaultBinding')) {
+                this.set('classNameBindings',
+                         ['parentView.isCustom:btn-warning:btn-success']);
+            }
+            this._super();
+        },
+        willInsertElement: function() {
+            this.set('parentView.labelFor', this.get('elementId'));
+        }
+    }),
+
+    actions: {
+        reset: function() {
+            this.set('value', this.get('default'));
+        }
     }
-    // layoutName: 'components/labelled-input-woA-woC',
 });
 
 Ember.Handlebars.helper('labelled-input', App.LabelledInputComponent);
