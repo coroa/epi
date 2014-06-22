3831a21c4b6df9b6b4fd714d68a178620563c06b
Jonas Hoersch <jonas@chaoflow.net>
2014-06-22 22:26:31 +0200

    unbound-select as a faster Ember.Select replacement
    without binding on the content (changes on content, lead to a full
    rerender of the select).

---
+++ app/components/unbound-select.js
@@ -0,0 +1,74 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tagName: "select",
+    optionValuePath: "content",
+    optionLabelPath: "content",
+    value: Ember.computed(function(key, value, oldValue) {
+        if (arguments.length !== 1) {
+            if (value != oldValue) {
+                this.updateSelectionFromValue(value);
+            }
+            return value;
+        }
+        var valuePath = this.get('optionValuePath').replace(/^content\.?/, '');
+        return valuePath ? this.get('selection.' + valuePath) : this.get('selection');
+    }).property('selection'),
+    update: function() {
+        this.rerender();
+    }.observes('content', 'selection'),
+    render: function(buffer) {
+        var labelKey = this.get("optionLabelPath")
+                .replace("content.", ""),
+            valueKey = this.get("optionValuePath")
+                .replace("content.", "");
+
+        var output = "";
+        if (this.get("prompt")) {
+            output += "<option>" + this.get("prompt") + "</option>";
+        }
+        output += this.get("content").map(function(obj){
+            var value = valueKey === "content" ? obj : Ember.get(obj, valueKey),
+                label = labelKey === "content" ? obj : Ember.get(obj, labelKey),
+                selected = this.get('selection') === obj;
+            return "<option" +
+                " value='"+value+"'" +
+                (selected ? " selected" : "") +
+                ">" +
+                label +
+                "</option>";
+        }, this).join("");
+
+        buffer.push(output);
+    },
+    updateSelectionFromValue: function(value) {
+        var content = this.get('content'),
+            valuePath = this.get('optionValuePath').replace(/^content\.?/, ''),
+            selectedValue = (valuePath ?
+                             this.get('selection.' + valuePath)
+                             : this.get('selection')),
+            selection;
+
+        if (value !== selectedValue) {
+            selection = content ? content.find(function(obj) {
+                return value === (valuePath ? Ember.get(obj,valuePath) : obj);
+            }) : null;
+
+            this.set('selection', selection);
+        }
+    },
+    _change: function() {
+        var selectedIndex = this.$()[0].selectedIndex,
+            content = this.get('content'),
+            prompt = this.get('prompt');
+
+        if (Ember.isEmpty(content)) { return; }
+        if (prompt && selectedIndex === 0) { this.set('selection', null); return; }
+        if (prompt) { selectedIndex -= 1; }
+        this.set('selection', content.objectAt(selectedIndex));
+    },
+    init: function() {
+        this._super();
+        this.on("change", this, this._change);
+    }
+});
