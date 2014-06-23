34f0b592b7bb80ca7bbf593df473ecc066b613e1
Jonas Hoersch <jonas@chaoflow.net>
2014-06-23 17:21:26 +0200

    fix chart2 without data

--- app/controllers/chart2.js
+++ app/controllers/chart2.js
@@ -6,8 +6,12 @@ export default Em.Controller.extend({
     data: function() {
         var levels = this.get('controllers.levels');
         return [].concat.apply([], levels.map(function(level) {
-            var level_name = level.get('name');
-            return level.get('storage_volume').map(function(d) {
+            var level_name = level.get('name'),
+                storage_volume = level.get('storage_volume');
+
+            if(Em.isEmpty(storage_volume)) { return []; }
+
+            return storage_volume.map(function(d) {
                 var sublabels =
                         d.get('items').mapBy('requirement.vaccine.label'),
                     values =
