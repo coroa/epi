1d8c695514c4673d022b558e28d93784b29ea251
Jonas Hoersch <jonas@chaoflow.net>
2014-06-30 17:22:23 +0200

    fix chart1's and chart2's category support
    both use reduceComputed now. so computational effort is on the order of
    1. instead of n.

--- app/controllers/chart1.js
+++ app/controllers/chart1.js
@@ -1,86 +1,6 @@
 import Em from 'ember';
 import Enums from '../enums';
 
-var do_stuff = function(accum) {
-    var queue = this.get('queue'), ch,
-        total = 0, newTotal = 0, updateMaxValue = false,
-        serviceObj, vaccineObj;
-    // propertyWill/DidChange block the browser indefinitely
-    this.propertyWillChange('data');
-    while((ch = queue.shiftObject()) !== null) {
-        if (ch.action === 'remove') {
-            // let's find the element first
-            serviceObj = accum.values.findBy('id', ch.service);
-            vaccineObj = serviceObj.values.findBy('id', ch.vaccine);
-            total = vaccineObj.total;
-
-            // peek ahead whether the next one adds the
-            // same element (thus it is only a change)
-            if (queue[0].action === 'add' &&
-                ch.index === queue[0].index) {
-                ch = queue.shiftObject();
-
-                console.log('doing an update of item', ch.index);
-            } else {
-                // remove them
-                serviceObj.values.removeObject(vaccineObj);
-                if (serviceObj.values.length === 0) {
-                    accum.values.removeObject(serviceObj);
-                }
-            }
-        } else if(ch.action === 'add') {
-            // find and create, if necessary
-            serviceObj = accum.values.findBy('id', ch.service);
-            if (serviceObj === undefined) {
-                serviceObj = Em.Object.create({ values: [] });
-                accum.values.pushObject(serviceObj);
-            }
-            vaccineObj = serviceObj.values.findBy('id', ch.vaccine);
-            if (vaccineObj === undefined) {
-                vaccineObj = Em.Object.create({ id: ch.vaccine });
-                serviceObj.values.pushObject(vaccineObj);
-            }
-        }
-
-        if (ch.action === 'add') {
-            // add or update
-            serviceObj.setProperties(
-                {id: ch.service,
-                 key: Enums.service
-                 .options[ch.service]['short']});
-            newTotal = ch.vaccine_volume + ch.diluent_volume;
-            vaccineObj.setProperties(
-                {key: ch.label,
-                 values: [{key: ch.label + ' - vaccine',
-                           value: ch.vaccine_volume},
-                          {key: ch.label + ' - diluent',
-                           value: ch.diluent_volume}],
-                 total: newTotal});
-        }
-
-        if (newTotal > total) {
-            if (newTotal > accum.maxValue) {
-                accum.set('maxValue', newTotal);
-            }
-        } else {
-            if (total === accum.maxValue) {
-                updateMaxValue = true;
-            }
-        }
-    }
-
-    if (updateMaxValue) {
-        accum.maxValue = accum.values.reduce(function(prev, cur) {
-            return Math.max(prev, cur.values.reduce(function(p, c) {
-                return Math.max(p, c.total);
-            }));
-        });
-    }
-    // propertyDidChange from a scheduled task blocks the browser
-    this.propertyDidChange('data');
-};
-
-
 export default Em.Controller.extend({
     needs: ['requirements'],
     staticDataLabels: true,
@@ -94,33 +14,73 @@ export default Em.Controller.extend({
             initialValue: function() {
                 return Em.Object.create({ maxValue: 0, values: []});
             },
-            initialize: function() { this.set('queue', []); },
             addedItem: function(accum, req, changeMeta) {
                 var label = req.get('vaccine.initials') +
-                        ' (' + req.get('vaccine.doses_per_vial') + ')';
-                this.get('queue').pushObject({
-                    action: 'add',
-                    index: changeMeta.index,
-                    label: label,
-                    service: req.get('service'),
-                    vaccine: req.get('id'),
-                    vaccine_volume: req.get('vaccine_volume_per_course'),
-                    diluent_volume: req.get('diluent_volume_per_course')
-                });
-                Em.run.scheduleOnce('actions', this, do_stuff, accum);
+                        ' (' + req.get('vaccine.doses_per_vial') + ')',
+                    service = req.get('service'),
+                    vaccine = req.get('id'),
+                    vaccine_volume = req.get('vaccine_volume_per_course'),
+                    diluent_volume = req.get('diluent_volume_per_course');
 
+                this.propertyWillChange('data');
+
+                var serviceObj = accum.values.findBy('id', service);
+                if (serviceObj === undefined) {
+                    serviceObj = Em.Object.create({ values: [] });
+                    accum.values.pushObject(serviceObj);
+                }
+                var vaccineObj = serviceObj.values.findBy('id', vaccine);
+                if (vaccineObj === undefined) {
+                    vaccineObj = Em.Object.create({ id: vaccine });
+                    serviceObj.values.pushObject(vaccineObj);
+                }
+                serviceObj.setProperties(
+                    {id: service,
+                     key: Enums.service
+                     .options[service]['short']});
+                var total = vaccine_volume + diluent_volume;
+                vaccineObj.setProperties(
+                    {key: label,
+                     values: [{key: label + ' - vaccine',
+                               value: vaccine_volume},
+                              {key: label + ' - diluent',
+                               value: diluent_volume}],
+                     total: total});
+
+                if (total > accum.maxValue) {
+                    accum.set('maxValue', total);
+                }
+
+                this.propertyDidChange('data');
                 return accum;
             },
             removedItem: function(accum, req, changeMeta) {
                 if (!changeMeta.previousValues) { return accum; }
-                this.get('queue').pushObject({
-                    action: 'remove',
-                    index: changeMeta.index,
-                    vaccine: changeMeta.previousValues.id
+
+                var vaccine = changeMeta.previousValues.id
                         || req.get('id'),
-                    service: changeMeta.previousValues.service
-                        || req.get('service') });
-                Em.run.scheduleOnce('actions', this, do_stuff, accum);
+                    service = changeMeta.previousValues.service
+                        || req.get('service'),
+                    serviceObj = accum.values.findBy('id', service),
+                    vaccineObj = serviceObj.values.findBy('id', vaccine),
+                    total = vaccineObj.total;
+
+                this.propertyWillChange('data');
+
+                // remove them
+                serviceObj.values.removeObject(vaccineObj);
+                if (serviceObj.values.length === 0) {
+                    accum.values.removeObject(serviceObj);
+                }
+
+                if (total === accum.maxValue) {
+                    accum.set('maxValue', accum.values.reduce(function(prev, cur) {
+                        return Math.max(prev, cur.values.reduce(function(p, c) {
+                            return Math.max(p, c.total);
+                        }));
+                    }));
+                }
+                this.propertyDidChange('data');
 
                 return accum;
             }
--- app/controllers/chart2.js
+++ app/controllers/chart2.js
@@ -1,57 +1,148 @@
 import Em from 'ember';
 import Enums from '../enums';
 
+var insertSortedBy = function(array, item, key) {
+    var value = item.get(key), i=0;
+    while (i < array.length && array.objectAt(i).get(key) < value) { i ++; }
+    return array.insertAt(i, item);
+},
+    formatter = {
+    level: function(id) { return id; },
+    service: function(id) { return Enums.service.options[id].label; },
+    temperature: function(id) {
+        return Enums.temperature.options[id].label;
+    },
+    vaccine: function(vac) {
+        return vac.get('initials') +
+            ' (' + vac.get('doses_per_vial') + ')';
+    }
+};
+
 export default Em.Controller.extend({
     needs: ['levels'],
+    categories: true,
     staticDataLabels: true,
-    // data: function() {
-    //     var levels = this.get('controllers.levels');
-    //     return [].concat.apply([], levels.map(function(level) {
-    //         var level_name = level.get('name'),
-    //             storage_volume = level.get('storage_volume');
 
-    //         if(Em.isEmpty(storage_volume)) { return []; }
+    // level, service, temperature can take as selection:
+    // one of 'primary', 'secondary', 'stack' or a concrete value,
+    // the filter property collects all concrete values
+    // input
+    level: 'primary',
+    service: 0,
+    temperature: 'secondary',
 
-    //         return storage_volume.map(function(d) {
-    //             var sublabels =
-    //                     d.get('items').mapBy('requirement.vaccine.label'),
-    //                 values =
-    //                     d.get('items').mapBy('storage_volume');
-    //             return { id: d.get('service')+'_'+level_name+'_'+d.get('temperature'),
-    //                      category: Enums.service.options[d.get('service')].label,
-    //                      label: level_name + " - " + Enums.temperature.options[d.get('temperature')].label,
-    //                      sublabels: sublabels,
-    //                      values: values };
-    //         }, this);
-    //     }, this));
-    // }.property('controllers.levels.@each.storage_volume')
-    data: { hasTopCategory: true,
-            maxValue: 6,
-            values: [
-                { key: "Level 1",
-                  values: [
-                      { key: "- 25 C",
-                        colour: Enums.temperature.options[0].colour,
-                        values: [
-                            { key: "DTP (10)",
-                              value: 1 },
-                            { key: "DTP-Hib",
-                              value: 4 } ],
-                        total: 5 },
-                      { key: "+ 5 C",
-                        colour: Enums.temperature.options[1].colour,
-                        values: [
-                            { key: "BCG (5)",
-                              value: 3 } ],
-                        total: 3 } ] },
-                { key: "Level 2",
-                  values: [
-                      { key: "- 25 C",
-                        colour: Enums.temperature.options[0].colour,
-                        values: [
-                            { key: "DTP (10)",
-                              value: 2 },
-                            { key: "BCG (5)",
-                              value: 4 } ],
-                        total: 6 } ] } ] }
+    // output
+    primary: 'level',
+    secondary: 'temperature',
+    stack: 'vaccine',
+    filter: function() {
+        var states = [ 'primary', 'secondary', 'stack' ], f = [];
+        ['level', 'service', 'temperature'].map(function(p) {
+            if (! states.contains(this.get(p))) {
+                f.push({ key: p, value: this.get(p) });
+            }
+        }, this);
+        return f;
+    }.property('level', 'service', 'temperature'),
+    data: Em.reduceComputed(
+        'controllers.levels.storage_volume',
+        {
+            initialValue: function() {
+                return Em.Object.create({ maxValue: 0, values: []});
+            },
+            addedItem: function(accum, item, changeMeta) {
+                if (this.get('filter').any(function(f) {
+                    return item[f.key] != f.value;
+                })) {
+                    return accum;
+                }
+
+                var primary = item.get(this.get('primary')),
+                    secondary = item.get(this.get('secondary')),
+                    stack = item.get(this.get('stack'));
+
+                this.propertyWillChange('data');
+
+                var primaryObj = accum.values.findBy('id', primary);
+                if (primaryObj === undefined) {
+                    primaryObj = Em.Object.create({ id: primary, values: [] });
+                    insertSortedBy(accum.values, primaryObj, 'id');
+                }
+                primaryObj.set('key',
+                               formatter[this.get('primary')](primary));
+
+                var secondaryObj = primaryObj.values.findBy('id', secondary);
+                if (secondaryObj === undefined) {
+                    secondaryObj = Em.Object.create({ id: secondary,
+                                                      values: [] });
+                    insertSortedBy(primaryObj.values, secondaryObj, 'id');
+                }
+                secondaryObj.set('key',
+                                 formatter[this.get('secondary')](secondary));
+                secondaryObj.incrementProperty('total', item.storage_volume);
+
+                insertSortedBy(secondaryObj.values,
+                               Em.Object.create(
+                                   { key: formatter[this.get('stack')](stack),
+                                     value: item.storage_volume}),
+                               'key');
+
+
+                if (secondaryObj.total > accum.maxValue) {
+                    accum.set('maxValue', secondaryObj.total);
+                }
+
+                this.propertyDidChange('data');
+                return accum;
+            },
+            removedItem: function(accum, item, changeMeta) {
+                if (!changeMeta.previousValues) { return accum; }
+
+                if (this.get('filter').any(function(f) {
+                    return ((changeMeta.previousValues[f.key] || item[f.key])
+                            != f.value);
+                })) {
+                    return accum;
+                }
+
+                var primary = changeMeta.previousValues[this.get('primary')]
+                        || item.get(this.get('primary')),
+                    secondary = changeMeta.previousValues[this.get('secondary')]
+                        || item.get(this.get('secondary')),
+                    stack = changeMeta.previousValues[this.get('stack')]
+                        || item.get(this.get('stack')),
+                    storage_volume = changeMeta.previousValues.storage_volume
+                        || item.get('storage_volume'),
+                    primaryObj = accum.values.findBy('id', primary),
+                    secondaryObj = primaryObj.values.findBy('id', secondary),
+                    stackObj = secondaryObj.values
+                        .findBy('key', formatter[this.get('stack')](stack)),
+                    total = secondaryObj.total;
+
+                this.propertyWillChange('data');
+
+                // remove them
+                secondaryObj.values.removeObject(stackObj);
+                if (secondaryObj.values.length === 0) {
+                    primaryObj.values.removeObject(secondaryObj);
+                    if (primaryObj.values.length === 0) {
+                        accum.values.removeObject(primaryObj);
+                    }
+                } else {
+                    secondaryObj.decrementProperty('total', storage_volume);
+                }
+
+                if (total === accum.maxValue) {
+                    accum.set('maxValue', accum.values.reduce(function(prev, cur) {
+                        return Math.max(prev, cur.values.reduce(function(p, c) {
+                            return Math.max(p, c.total);
+                        }));
+                    }));
+                }
+
+                this.propertyDidChange('data');
+
+                return accum;
+            }
+        })
 });
