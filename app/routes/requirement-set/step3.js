4673b891ad879e70b6abb5f7f0af6dd47e7661f1
Jonas Hoersch <jonas@chaoflow.net>
2014-08-11 16:10:15 +0200

    Fix step3 to wait until the facilities are loaded

--- app/routes/requirement-set/step3.js
+++ app/routes/requirement-set/step3.js
@@ -1,19 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    setupController: function() {
-        this._super();
+    setupController: function(controller, model) {
+        this._super(controller, {});
         this.controllerFor('chart-picker')
             .send('switchToChart', null);
-        var facilities = this.controllerFor('facilities');
-        if (facilities.get('length') === 0) {
-            console.log('loading facilities');
-            facilities.set('model', this.store.find('facility', {paging: false}));
+        if (model !== null) {
+            this.controllerFor('facilities').set('model', model);
         }
-    }
+    },
     // actions: {
     //     setAffected: function(ps, _, affected) {
     //         this.controllerFor('requirements').setAffectedParamset(ps, affected);
     //     }
     // }
+    model: function() {
+        var facilities = this.controllerFor('facilities');
+        if (facilities.get('length') === 0) {
+            console.log('loading facilities');
+            return this.store.find('facility', {paging: false});
+        } else {
+            return null;
+        }
+    }
 });
