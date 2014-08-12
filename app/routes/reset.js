9ec7e45efb44b45dc39fbcbbedbc01147bc96700
Jonas Hoersch <jonas@chaoflow.net>
2014-08-12 13:13:24 +0200

    have reset relocate to the index page after resetting

--- app/routes/reset.js
+++ app/routes/reset.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    redirect: function() {
+        window.location.replace("http://localhost:4200/");
+    },
     beforeModel: function() {
         var adapter = Ember.lookup.lookup("adapter:application");
         adapter.resetData();
