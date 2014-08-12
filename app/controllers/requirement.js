e47f0eab37f422e1050ed23853f3d26d01dabc5d
Jonas Hoersch <jonas@chaoflow.net>
2014-08-12 13:08:20 +0200

    Using a fully recomputing property for the storage_volume on requirement
    instead of the one-time semantics, as they get confused.

--- app/controllers/requirement.js
+++ app/controllers/requirement.js
@@ -10,33 +10,104 @@ export default Em.ObjectController.extend({
     }.property('vaccineInitials',
                'controllers.vaccines.@each.initials'),
 
-    storage_volume: Em.computed.map(null, function(ps) {
+
+    storage_volume: function() {
         var service = this.get('service'),
-            vaccine = this.get('vaccine');
-        // console.log('updating storage_volume of',
-        //             Enums.service.options[service].word,
-        //             'requirement', vaccine.get('initials'), 'to',
-        //             ps.get('storage_volume'));
-        // console.log('are there observers? ',
+            vaccine = this.get('vaccine'),
+            requirementId = this.get('id');
+
+        // console.log('are there observers for me? ',
         //             this.get('storage_volume.hasArrayObservers') ?
         //             'yes' : 'no');
+
+        return this.get('levelParamsets').map(function(ps) {
+            // console.log('updating storage_volume of',
+            //             Enums.service.options[service].word,
+            //             'requirement', vaccine.get('initials'), 'to',
+            //             ps.get('storage_volume'));
+            console.log('am i observing the storage volumes on the'
+                        + ' levelParamset? ',
+                        ps.hasObserverFor('storage_volume') ? 'yes' : 'no');
         // console.log('updating storage_volume on requirement model with'
         //             + ' level', ps.get('level'), 'of name', ps.get('level.name'));
-        if (! ps.get('level.isLoaded')) {
-            return Em.Object.create();
-        }
+            if (! ps.get('level.isLoaded')) {
+                debugger;
+                return Em.Object.create();
+            }
 
-        return Em.Object.create(
-            { level: ps.get('level'),
-              temperature: ps.get('temperature'),
-              service: service,
-              vaccine: vaccine,
-              storage_volume: ps.get('storage_volume'),
-              requirementId: this.get('id'),
-              paramsetId: ps.get('id') });
-    }).property('levelParamsets.@each.{temperature,storage_volume,level}',
-                'service', 'vaccine', 'levelParamsets.[]',
-                'controllers.levels.[]'), // set the dependent key omitted earlier
+            return Em.Object.create(
+                { level: ps.get('level'),
+                  temperature: ps.get('temperature'),
+                  service: service,
+                  vaccine: vaccine,
+                  storage_volume: ps.get('storage_volume'),
+                  requirementId: requirementId,
+                  paramsetId: ps.get('id') });
+        });
+    }.property('levelParamsets.@each.{temperature,storage_volume,level}',
+               'service', 'vaccine',
+               'controllers.levels.[]'), // set the dependent key omitted earlier
+
+    // storage_volume: Em.computed.map('levelParamsets.@each.{temperature,storage_volume,level}', function(ps) {
+    //     var service = this.get('service'),
+    //         vaccine = this.get('vaccine');
+    //     // console.log('updating storage_volume of',
+    //     //             Enums.service.options[service].word,
+    //     //             'requirement', vaccine.get('initials'), 'to',
+    //     //             ps.get('storage_volume'));
+    //     console.log('are there observers for me? ',
+    //                 this.get('storage_volume.hasArrayObservers') ?
+    //                 'yes' : 'no');
+    //     console.log('am i observing the storage volumes on the'
+    //                 + ' levelParamset? ',
+    //                 ps.hasObserverFor('storage_volume') ? 'yes' : 'no');
+    //     // console.log('updating storage_volume on requirement model with'
+    //     //             + ' level', ps.get('level'), 'of name', ps.get('level.name'));
+    //     if (! ps.get('level.isLoaded')) {
+    //         debugger;
+    //         return Em.Object.create();
+    //     }
+
+    //     return Em.Object.create(
+    //         { level: ps.get('level'),
+    //           temperature: ps.get('temperature'),
+    //           service: service,
+    //           vaccine: vaccine,
+    //           storage_volume: ps.get('storage_volume'),
+    //           requirementId: this.get('id'),
+    //           paramsetId: ps.get('id') });
+    // }).property('levelParamsets.@each.{temperature,storage_volume,level}',
+    //             'service', 'vaccine',
+    //             'controllers.levels.[]',
+    //             'levelParamsets.[]'), // set the dependent key omitted earlier
+
+    // storage_volume: Em.arrayComputed(
+    //     'levelParamsets.@each.{temperature,level,storage_volume}',
+    //     'service', 'vaccine', 'controllers.levels',
+    //     {
+    //         addedItem: function(accum, ps, changeMeta) {
+    //             console.log('addedItem in storage_volume on requirement');
+    //             if (! ps.get('level.isLoaded')) {
+    //                 return accum;
+    //             }
+    //             accum.replace(changeMeta.index, 0, [
+    //                 Em.Object.create(
+    //                     { level: ps.get('level'),
+    //                       temperature: ps.get('temperature'),
+    //                       service: this.get('service'),
+    //                       vaccine: this.get('vaccine'),
+    //                       storage_volume: ps.get('storage_volume'),
+    //                       requirementId: this.get('id'),
+    //                       paramsetId: ps.get('id') })
+    //             ]);
+    //             return accum;
+    //         },
+    //         removedItem: function(accum, item, changeMeta) {
+    //             accum.replace(changeMeta.index, 1, []);
+    //             return accum;
+    //         }
+    //     }
+    // ),
 
     data: function() {
         var reqs = this.get("controllers.requirements");
