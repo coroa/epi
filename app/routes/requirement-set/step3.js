10dcda62084c29ddf782596bd0ebd73433d8dc96
Jonas Hoersch <jonas@chaoflow.net>
2014-08-14 11:15:29 +0200

    Load facility population and capacities, eagerly, all at once

--- app/routes/requirement-set/step3.js
+++ app/routes/requirement-set/step3.js
@@ -5,8 +5,8 @@ export default Ember.Route.extend({
         this._super(controller, {});
         this.controllerFor('chart-picker')
             .send('switchToChart', null);
-        if (model !== null) {
-            this.controllerFor('facilities').set('model', model);
+        if (model.facilities !== undefined) {
+            this.controllerFor('facilities').set('model', model.facilities);
         }
     },
     // actions: {
@@ -15,12 +15,19 @@ export default Ember.Route.extend({
     //     }
     // }
     model: function() {
-        var facilities = this.controllerFor('facilities');
+        var models = {},
+            facilities = this.controllerFor('facilities'),
+            dataValues = this.store.all('data-value');
         if (facilities.get('length') === 0) {
             console.log('loading facilities');
-            return this.store.find('facility', {paging: false});
-        } else {
-            return null;
+            models.facilities = this.store.find('facility', {paging: false});
         }
+
+        if (dataValues.get('length') === 0) {
+            console.log('loading data-values');
+            models.dataValues = this.store.find('data-value');
+        }
+
+        return Ember.RSVP.hash(models);
     }
 });
--- app/serializers/data-value.js
+++ app/serializers/data-value.js
@@ -11,6 +11,27 @@ export default DS.JSONSerializer.extend({
             });
         return ids.map(function(id) { return objs[id] || { id: id }; });
     },
+    extractFindAll: function(store, type, payload) {
+        var records = this.extractArray(store, type, payload),
+            includedIds = records.mapBy('id'),
+            pe = payload.metaData.pe,
+            ou = payload.metaData.ou,
+            de = this.dhis.getDEs(),
+            i, i_length, j, j_length, k, k_length, id;
+        // add the rest of the data
+        for(i=0, i_length=pe.length; i<i_length; i++) {
+            for(j=0, j_length=ou.length; j<j_length; j++) {
+                for (k=0, k_length=de.length; k<k_length; k++) {
+                    id = this.dhis.buildIdFromQuery(pe[i], ou[j], de[k]);
+                    if (! includedIds.contains(id)) {
+                        records.push({ id: id, value: null });
+                    }
+                }
+            }
+        }
+
+        return records;
+    },
     extractArray: function(store, type, payload) {
         return payload["rows"].map(function(r) {
             return this.normalize(type, r);
