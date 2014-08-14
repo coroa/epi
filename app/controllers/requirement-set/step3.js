1325e44cd181890162d13a60e9f42227f24fc0f0
Jonas Hoersch <jonas@chaoflow.net>
2014-08-14 21:35:25 +0200

    Add comments to step3 controller

--- app/controllers/requirement-set/step3.js
+++ app/controllers/requirement-set/step3.js
@@ -2,12 +2,41 @@ import Ember from 'ember';
 import Enums from '../../enums';
 
 export default Ember.Controller.extend({
-    selectedChildren: Ember.computed.oneWay('selected.wrappedChildren'),
     needs: ['requirements', 'facilities', 'levels'],
+
+
+    /**
+     * `selectedAsArray` wraps the selected facility into an array, so
+     * it can be fed into a surplus-chart. This shows the left of the
+     * two clustered bar charts.
+     *
+     * @property selectedAsArray
+     * @type Array of FacilityController
+     */
     selectedAsArray: function() {
         return this.get('selected') ? [ this.get('selected') ] : null;
     }.property('selected'),
+
+    /**
+     * `selectedChildren` holds a list of the facilities, which are
+     * children of the selected. They are shown in the right of the
+     * two clustered bar plots, surplus-chart.
+     *
+     * @property selectedChildren
+     * @type Array of FacilityController
+     */
+    selectedChildren: Ember.computed.oneWay('selected.wrappedChildren'),
+
+    /**
+     * `selected` is bound to the facility tree on the left. And
+     * controls on which facility the focus of the step3 should be.
+     *
+     * @property selected
+     * @type FacilityController
+     * @default facility with level.id=1
+     */
     selected: Ember.computed.oneWay('country'),
+
     country: function() {
         return this.get('controllers.facilities').findBy('level.id', '1');
     }.property('controllers.facilities.@each.level'),
