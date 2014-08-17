653578fe43175307ae1c9a2ea48e6a85516062ca
Jonas Hoersch <jonas@chaoflow.net>
2014-08-17 20:23:45 +0200

    Rename the number property for rounded-number to content

--- app/components/rounded-number.js
+++ app/components/rounded-number.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    number: 0,
+    content: 0,
     digits: 2,
     roundedNumber: function() {
         var factor = Math.pow(10, this.get('digits'));
-        return Math.round(this.get('number') * factor) / factor;
-    }.property('number')
+        return Math.round(this.get('content') * factor) / factor;
+    }.property('content')
 });
