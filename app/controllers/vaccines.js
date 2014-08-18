05291003fcf12dc3b4a20dc96b45fe17217a9584
Jonas Hoersch <jonas@chaoflow.net>
2014-08-18 14:44:20 +0200

    Sort the vaccines by initials

--- app/controllers/vaccines.js
+++ app/controllers/vaccines.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
     itemController: 'vaccine',
+    sortProperties: ['initials'],
     vaccineInitials: function() {
         return this.mapBy('initials').uniq();
     }.property('@each.initials')
